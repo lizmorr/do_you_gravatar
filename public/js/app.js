68c9f874ca96a8dd27446a6963011ac7d0ae51af
Liz Morrison <efmorr@gmail.com>
2015-03-13 16:20:15 -0400

    Add validation that the email address has an @ sign. Alert if it does not.

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,11 @@
 $("#gravatar_checker").on("submit", function (event) {
   event.preventDefault();
   var email=$("#email").val();
-  $.get("/email?email=" + email, function(getImage) {
-    $("div.picture").replaceWith("<div class=\"picture\"><img src=\"" + getImage + "\"></img></div>");
-  });
+  if(!/@/.test(email)) {
+    alert("Please enter a valid email!")
+  } else {
+    $.get("/email?email=" + email, function(getImage) {
+      $("div.picture").replaceWith("<div class=\"picture\"><img src=\"" + getImage + "\"></img></div>");
+    });
+  }
 });
